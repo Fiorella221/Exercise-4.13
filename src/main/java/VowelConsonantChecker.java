7714fa30f6ce514b88425dc458d496e033b23fb3
Fiorella221 <fiorella_njca@outlook.com>
2024-09-25 05:35:06 +0000

    Initial commit

---
+++ src/main/java/VowelConsonantChecker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VowelConsonantChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        String ch = input.next();
+
+        if ((ch.length() != 1) || !Character.isLetter(ch.charAt(0))) {  System.out.println("Invalid input");
+            return;
+       } else {  
+            char letter = ch.charAt(0);
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+
+System.out.println(letter + " is a vowel");
+                 } else {
+                    
+ System.out.println(letter + " is a consonant");
+                }
+            }
+            input.close();
+        }
+    }
+  
+    
+
+       
+       
